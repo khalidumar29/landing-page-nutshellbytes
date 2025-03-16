ec1b17111440ef160c5cd24084e8a83f2cda9258
Muhammad Khalid Umar <dreamiwoarld.online@gmail.com>
2025-03-17 02:02:59 +0600

    api create done

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import nodemailer from "nodemailer";
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
+  const { name, email, phone, subject, message } = req.body;
+  if (req.method === "POST") {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD,
+        },
+      });
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: subject,
+        text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      return res.status(200);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  if (req.method === "GET") {
+    return res.status(200);
+  }
 }
